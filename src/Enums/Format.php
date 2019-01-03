f1810350eae577a14b8e43d0f6cc5d7bd424755c
Jordan Hall <jordan@hall05.co.uk>
2019-01-03 21:01:28 +0000

    Initial commit

---
+++ src/Enums/Format.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DivineOmega\WikipediaInfoBoxParser\Enums;
+
+abstract class Format
+{
+    const PLAIN_TEXT = 'plain_text';
+    const HTML = 'html';
+    const MARKDOWN = 'markdown';
+}
---
+++ src/WikipediaInfoBoxParser.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace DivineOmega\WikipediaInfoBoxParser;
+
+use DivineOmega\WikipediaInfoBoxParser\Enums\Format;
+
+class WikipediaInfoBoxParser
+{
+    private $article;
+    private $format;
+
+    private $endpoint = 'https://en.wikipedia.org/w/api.php';
+    private $queryString = '?format=json&action=query&prop=revisions&rvprop=content&rvslots=main&titles=';
+
+    /**
+     * Sets the Wikipedia article you wish to parse the info box for.
+     *
+     * @param mixed $article
+     * @return WikipediaInfoBoxParser
+     */
+    public function setArticle(string $article) : WikipediaInfoBoxParser
+    {
+        $this->article = $article;
+        return $this;
+    }
+
+    /**
+     * Sets the format you wish the values to parsed into.
+     *
+     * @see Format
+     *
+     * @param mixed $format
+     * @return WikipediaInfoBoxParser
+     */
+    public function setFormat(string $format) : WikipediaInfoBoxParser
+    {
+        $this->format = $format;
+        return $this;
+    }
+
+    private function buildUrl()
+    {
+        return $this->endpoint.$this->queryString.$this->article;
+    }
+
+    public function parse()
+    {
+        $url = $this->buildUrl();
+
+        $data = json_decode(file_get_contents($url), true);
+        $content = $data['query']['pages'][24131]['revisions'][0]['slots']['main']['*'];
+    }
+}
